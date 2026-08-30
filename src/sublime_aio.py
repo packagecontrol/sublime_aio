packagecontrol/sublime_aio

commit 9bdfa57ee10c029022943c0b6b1502e1a2c54164

Fix python 3.8 compatibility issue
This commit creates the loop by `get_event_loop()` instead of `new_event_loop()`
as the latter one caused first call of `asyncio.gather()` to raise exception
due to internal callback being attached to wrong thread.

src/sublime_aio.py
@@ -66,7 +66,7 @@ __version__ = "0.2.5"
 try:
     _loop, _tasks, _thread = sublime_plugin._sublime_aio_globals
 except AttributeError:
-    _loop = asyncio.new_event_loop()
+    _loop = asyncio.get_event_loop()
     _loop.set_default_executor(
         concurrent.futures.ThreadPoolExecutor(thread_name_prefix="sublime_aio.worker")
     )

commit a674ed7c3a11f0578df3eca9042c4ac6e6192945

Fix python 3.8 vs. 3.14 incompatibilities
Fixes #40

This commit fixes a regression for python 3.14, introduced by 9bdfa57,
which causes a runtime error due to `asyncio.get_event_loop()` no longer
automatically creating new loops.

It appears calling `asyncio.set_event_loop()` fixes exceptions on python 3.8
which 9bdfa57 intended to fix. It appears various asyncio internals rely on
the event loop being assigned via `asyncio.set_event_loop()`.

src/sublime_aio.py
@@ -66,10 +66,11 @@ __version__ = "0.2.6"
 try:
     _loop, _tasks, _thread = sublime_plugin._sublime_aio_globals
 except AttributeError:
-    _loop = asyncio.get_event_loop()
+    _loop = asyncio.new_event_loop()
     _loop.set_default_executor(
         concurrent.futures.ThreadPoolExecutor(thread_name_prefix="sublime_aio.worker")
     )
+    asyncio.set_event_loop(_loop)
     _tasks = set()
     _thread = Thread(target=_loop.run_forever, name="sublime_aio", daemon=True)
     _thread.start()
@@ -142,6 +143,7 @@ def on_exit(log_path: str):
         _thread.join()
         loop.run_until_complete(loop.shutdown_asyncgens())
         loop.close()
+        asyncio.set_event_loop(None)
 
     # write stdout to provided logfile
     if len(stdout.getvalue()):